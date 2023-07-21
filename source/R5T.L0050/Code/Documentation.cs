169a3fd15d7ea72a2ae53409fffedbfe5417a723
davidcoats <david@getsafetycone.com>
2023-07-21 10:07:22 -0700

    Initial commit.

---
+++ source/R5T.L0050/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.L0050
+{
+	/// <summary>
+	/// Dropbox specific functionality library (Dropbox directory path, etc.)
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.L0050/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.L0050
+Dropbox specific functionality library (Dropbox directory path, etc.)
